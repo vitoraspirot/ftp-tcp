6d133e857863542baa555b92368c465ed02e36b1
Vitor Aspirot <vitoraspirot@gmail.com>
2021-11-09 23:37:57 -0300

    adiciona funcao download

--- ftp-client/client.c
+++ ftp-client/client.c
@@ -10,7 +10,7 @@
 #define PORT 8080
 #define IP "192.168.0.110"
 #define SA struct sockaddr
-#define DOWNLOAD_ARG 0
+#define DOWNLOAD_ARG 2
 #define UPLOAD_ARG 1
 
 void upload_to_server(int sockfd, char fileName[50]);
@@ -22,25 +22,30 @@ int main(int argc, char const *argv[])
 	int transferArgument;
     FILE *arquivo;
     char fileName[30];
-    transferArgument = 0;
+    char transferType[30];
 
     if(argc!=3){
-        printf(">> Error! Invalid argument number.\n");
+        printf(">> Error! Invalid number of arguments.\n");
         return 0;
     }
-    if(strcmp(argv[1],"upload")!=0&&strcmp(argv[1],"download")!=0){
-        printf(">> Error! Invalid transfer argument.\n");
-        return 0;
-    }
+
+    bzero(transferType, 30);
+    bzero(fileName, 30);
+    strcpy(transferType, argv[1]);
     strcpy(fileName, argv[2]);
-    if(strcmp(argv[1],"upload")==0){
-        transferArgument=1;
+
+    if(strcmp(transferType, "1")==0){
+        transferArgument = UPLOAD_ARG;
         arquivo = fopen(fileName, "r");
         if(arquivo==NULL){
-            printf(">> Error! The file doesn't exist.\n");
             fclose(arquivo);
             return 0;
         }
+    }else if(strcmp(transferType, "2")==0){
+        transferArgument = DOWNLOAD_ARG;
+        printf("%d - ", DOWNLOAD_ARG);
+    }else{
+        return 0;
     }
     fclose(arquivo);
     connect_to_server(transferArgument, fileName);
@@ -49,7 +54,7 @@ int main(int argc, char const *argv[])
 
 void upload_to_server(int sockfd, char fileName[50]){
 	char buff[BUFFER_SIZE];
-    int i, rval;
+    int i;
     char character;
     FILE *arquivo;
     arquivo = fopen(fileName, "r");
@@ -74,7 +79,7 @@ void upload_to_server(int sockfd, char fileName[50]){
             i++;
         }while(character!=EOF);
         printf("%s\n", buff);
-        send(sockfd, &buff, sizeof(buff), 0);
+        write(sockfd, buff, sizeof(buff));
         i=0;
         bzero(buff, BUFFER_SIZE);
     }	
@@ -115,5 +120,33 @@ void connect_to_server(int transferArgument, char fileName[50]){
 }
 
 void download_from_server(int sockfd, char fileName[50]){
+    char buff[BUFFER_SIZE];
+    int i;
+    char character;
+    FILE *arquivo;
+    bzero(buff, BUFFER_SIZE);
+    strcpy(buff, "download");
+    write(sockfd, buff, sizeof(buff));
 
+    bzero(buff, BUFFER_SIZE);
+    strcpy(buff, fileName);
+    write(sockfd, buff, sizeof(buff));
+
+    bzero(buff, BUFFER_SIZE);
+    read(sockfd, buff, sizeof(buff));
+    if(strcmp(buff, "OK")!=0){
+        printf(">> Error! The file doesn't exist.\n");
+    }else{
+        arquivo = fopen(fileName, "w");
+        i=0;
+        if(arquivo!=NULL){
+            while(i<1024){
+                bzero(buff, BUFFER_SIZE);
+                read(sockfd, buff, sizeof(buff));
+                fprintf(arquivo, "%s", buff);
+                i++;
+            }
+            fclose(arquivo);
+        }
+    }
 }
